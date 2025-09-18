adc49fcf81d7291bd6d4806262c5a30d776693c1
JOHN ALFIE RIZARRI -UCMN <johnrizarri187@gmail.com>
2025-09-18 08:18:50 +0800

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,17 @@
+public class Comments {
+    public static void main(String[] args) {
+        // This is a single-line comment
+
+        /*
+         This is a multi-line comment.
+         It can span several lines.
+        */
+
+        /**
+         * This is a Javadoc comment.
+         * It is used to generate documentation.
+         */
+
+        System.out.println("Java comments example displayed!");
+    }
+}
