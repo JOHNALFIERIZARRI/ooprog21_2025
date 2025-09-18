bd9a5b3fd52e4fd3bf0772e9f182611c789682b8
JOHN ALFIE RIZARRI -UCMN <johnrizarri187@gmail.com>
2025-09-18 08:43:54 +0800

    Delete FavoriteSong.java

--- FavoriteSong.java
+++
@@ -1,9 +0,0 @@
-public class FavoriteSong{
-   public static void main (String[]args){
-      System.out.println("kaleidoscope");
-      System.out.println("Pare Ko");
-      System.out.println("fall apart");
-      System.out.println("Bread");
-   }
-
-}
