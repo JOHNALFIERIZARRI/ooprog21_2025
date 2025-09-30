9666354b629619eb8eaa7b7e9a9c898a0a30513b
JOHN ALFIE RIZARRI -UCMN <johnrizarri187@gmail.com>
2025-09-30 09:03:37 +0800

    Add files via upload

--- Chapter_4/DebugFour1.java
+++ Chapter_4/DebugFour1.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class DebugFour1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int radius;
 
         System.out.print("Enter a radius for a circle >> ");
-        radius = input.nextInt();
+        int radius = input.nextInt();
 
-               Circle c = new DebugCircle(radius);
+        // Create a DebugCircle object using the input radius
+        Circle c = new DebugCircle(radius);
 
+        // Display the properties of the circle
         System.out.println("The radius is " + c.getRad());
         System.out.println("The diameter is " + c.getDiam());
         System.out.println("The area is " + c.getArea());
