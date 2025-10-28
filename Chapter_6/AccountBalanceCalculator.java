58f9c661b7a3652937ebc83a7de18760cf78a150
JOHN ALFIE RIZARRI -UCMN <johnrizarri187@gmail.com>
2025-10-28 22:46:06 +0800

    Add files via upload

---
+++ Chapter_6/AccountBalanceCalculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class AccountBalanceCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Enter initial bank balance > ");
+        double initialBalance = input.nextDouble();
+
+
+        double[] interestRates = {0.02, 0.03, 0.04, 0.05};
+
+        for (int i = 0; i < interestRates.length; i++) {
+            double rate = interestRates[i];
+            double balance = initialBalance;
+
+            System.out.printf("%nWith an initial balance of %.1f at an interest rate of %.2f%n", initialBalance, rate);
+
+          
+            for (int year = 1; year <= 4; year++) {
+                balance = balance + (balance * rate);
+                System.out.printf("After year %d balance is %.4f%n", year, balance);
+            }
+        }
+
+        input.close();
+    }
+}
